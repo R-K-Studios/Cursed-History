ae30be4d9196956d5b750ab82cb0f869b30e9e72
Kyrio5 <kyriobs@gmail.com>
2018-01-21 14:52:19 -0800

    Revert "Revert "Large evidence""

---
+++ Assets/SCRIPTS/EvidenceTable.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EvidenceTable : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public EvidenceBase evidenceRef;
+    public float slowestClickSpeed;
+    public float downTime;
+    private Vector2 touchOffset;
+    private Vector2 initPos;
+    private int initLayer;
+
+
+    private Vector2 CurrentTouchPosition() {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+    
+    private void SetDraggedPosition(PointerEventData data) {
+        Vector3 p = CurrentTouchPosition() + touchOffset;
+        p.z = -200;
+        transform.position = p;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        initLayer = gameObject.layer;
+        gameObject.layer = 2;
+        initPos = transform.position;
+        touchOffset = initPos - CurrentTouchPosition();
+        transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+        SetDraggedPosition(eventData);
+    }
+
+    public void OnDrag(PointerEventData data) {
+        SetDraggedPosition(data);
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        gameObject.layer = initLayer;
+        transform.localScale = new Vector3(1f, 1f, 1f);
+        transform.position = initPos;
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        downTime = Time.unscaledTime;
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        if (downTime != -1.0f && eventData.pointerPress != null && eventData.pointerPress.CompareTag("Evidence")) {
+            float upTime = Time.unscaledTime;
+            print(downTime);
+            print(upTime);
+            float clickSpeed = upTime - downTime;
+            print(clickSpeed);
+            print(slowestClickSpeed);
+            if (clickSpeed <= slowestClickSpeed) {
+                PopupManager.Instance.StartPopupAtState(evidenceRef.EnlargeID);
+            }
+        }
+        downTime = -1.0f;
+    }
+}
