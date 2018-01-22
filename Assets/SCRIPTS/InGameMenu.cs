b2cb52e1d4f79a1c47ed25c34f8b46410f2d9f4b
Kyrio5 <kyriobs@gmail.com>
2018-01-21 18:20:23 -0800

    Mergin Branches

---
+++ Assets/SCRIPTS/InGameMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMenu : MenuUI {
+
+    public OptionsPanel Opt;
+    public TransitionManager tm;
+    public PopupManager pop;
+    // Update is called once per frame
+    
+    void Update () {
+
+        if (Input.GetButtonDown("Cancel") && !pop.showPopup)
+        {
+            if (!IsActive())
+            {
+                SetActive();
+            }
+            else if (!Opt.IsActive())
+            {
+                SetInactive();
+            }
+        }
+	}
+
+
+    public void Options()
+    {
+        Opt.SetActive();
+    }
+
+    public void Save()
+    {
+
+    }
+    public void Load()
+    {
+
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void QuitToMenu()
+    {
+        SetInactive();
+        tm.TitleScreen();
+    }
+}
